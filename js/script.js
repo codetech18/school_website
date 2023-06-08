2cceea2afe87caa479f3a2c6ebc76264c5727aa4
codetech18 <akoredealao18@gmail.com>
2023-06-08 14:23:13 -0700

    connected databse to the login signup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxRSh6llNcGLKoXC3hldUTxpygW-N-xf8",
+    authDomain: "schoolwebsite-2c126.firebaseapp.com",
+    projectId: "schoolwebsite-2c126",
+    storageBucket: "schoolwebsite-2c126.appspot.com",
+    messagingSenderId: "961681962369",
+    appId: "1:961681962369:web:48ffad5b863876df9e2480"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let navbar = document.querySelector('.header .navbar')
 
 document.querySelector('#menu-btn').onclick = () => {
@@ -34,6 +63,64 @@ document.querySelector('#account-btn').onclick = () => {
 document.querySelector('#close-form').onclick = () => {
     accountForm.classList.remove('active');
 };
+
+
+// firebase auth code
+
+document.getElementById("login-btn").addEventListener('click', function(){
+    const loginEmail = document.getElementById("login-email").value;
+    const loginPassword = document.getElementById("login-password").value;
+
+    signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display='block';
+    registerBtn.style.display='none';
+    loginBtn.style.display='none';
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+    document.getElementById("result").innerHTML="Heyy, welcome back <br>" +loginEmail;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display='block';
+    registerBtn.style.display='none';
+    loginBtn.style.display='none';
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+    document.getElementById("result").innerHTML="sorry <br>" +errorMessage;
+  });
+});
+
+
+document.getElementById("register-btn").addEventListener('click', function(){
+    const registerEmail = document.getElementById("register-email").value;
+    const registerPassword = document.getElementById("register-password").value;
+
+    createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display='block';
+    registerBtn.style.display='none';
+    loginBtn.style.display='none';
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+    document.getElementById("result").innerHTML="welcome <br>" +registerEmail+ " registerd succesfully";
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display='block';
+    registerBtn.style.display='none';
+    loginBtn.style.display='none';
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+    document.getElementById("result").innerHTML="sorry <br>" +errorMessage;
+  });
+});
+
+
 
 var swiper = new Swiper(".home-slider", {
     pagination: {
